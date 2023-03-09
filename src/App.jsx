f96f16ba203a9f45366f42de4d215911b89b0335
LisethLira <liseth.lira.123@gmail.com>
2023-03-09 13:58:42 -0500

    promociones

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Promotions from './componentes/Promociones/Promotions' ;
+import './componentes/Promociones/promotionsStyle.css'
+function App() {
+  return (
+  <div>
+    <Promotions />;
+  </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Promociones/Promotions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import promotion1 from '../../img/promotion1.png';
+import './promotionsStyle.css'
+
+export default function promotions() {
+    return (
+      <div>
+        <h1>Promociones</h1>
+        <div className='image'>
+        <img src={promotion1} alt="promotion" className='image'/>
+        </div>
+      </div>
+    )
+  };
