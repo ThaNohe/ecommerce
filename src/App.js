64c1565a08896eda5e5d55e518897b92c984fabe
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2023-03-09 14:31:30 -0500

    Unido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import Header from './componentes/Header/Header';
 import Footer from './componentes/Footer/Footer'
+import Promotions from './componentes/Promociones/Promotions' ;
+import './componentes/Promociones/promotionsStyle.css'
 
 function App() {
   return (
     <>
     <Header/>
     <Footer/>
+     <Promotions/>
     </>
   );
 }
---
+++ src/componentes/Promociones/Promotions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import promotion1 from '../img/promotion1.png';
+import './promotionsStyle.css'
+
+//promociones de web
+export default function promotions() {
+    return (
+      <div>
+        <h1>Promociones</h1>
+        <div className='image'>
+        <img src={promotion1} alt="promotion" className='image'/>
+        </div>
+      </div>
+    )
+  };
